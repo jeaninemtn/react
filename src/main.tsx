02d8d23ecdb1edefe81377b43445fa35772225cf
jeaninemtn <jeaninemtn@gmail.com>
2025-12-08 14:07:03 +0800

    add createRoot() note

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,21 @@ createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router} />
   </StrictMode>,
 )
+
+/* `createRoot` 
+
+1. A built-in function that is used to create a root node for a React application.
+2. This function takes one argument, an HTML element.
+*/
+
+/* `render`
+Defines what to render in the HTML container.
+*/
+
+/* 程式最簡化後的樣子 
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <h1>Hello React!</h1>
+) 
+*/
