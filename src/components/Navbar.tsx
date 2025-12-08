7b9413a94046cab1b44b53ab0097a8c18e0f9492
jeaninemtn <jeaninemtn@gmail.com>
2025-12-08 13:47:02 +0800

    update navbar and background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,12 +10,15 @@ export default function Navbar() {
     <nav className="flex justify-between items-center p-4">
 
       <div className="flex items-center gap-4">
-        <Link to="/">Home</Link>
+
+        <Link to="/">
+          <i className="fa-solid fa-home"></i>
+        </Link>
         <Link to="/tictactoe">Tic Tac Toe</Link>
       </div>
 
       <button className="cursor-pointer" onClick={toggleTheme}>
-         <i className={ theme == 'light' ? 'fa-solid fa-moon text-(--text)' : 'fa-solid fa-cloud-sun text-(--text)'}></i>
+        <i className={theme == 'light' ? 'fa-solid fa-moon text-(--text)' : 'fa-solid fa-cloud-sun text-(--text)'}></i>
       </button>
     </nav>
   );
